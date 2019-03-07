d58d488f91eba3311cc82793656cc9484c539223
Yash Kulkarni <yculcarneee@gmail.com>
2019-03-08 01:00:21 +0530

    changed scenario to locha_home_1.wad

--- agent_a3c.py
+++ agent_a3c.py
@@ -23,7 +23,7 @@ test_display = False
 test_write_video = True
 path_work_dir = "rl_3d/"
 vizdoom_path = "ViZDoom/"
-vizdoom_scenario = vizdoom_path + "scenarios/locha_home.wad"
+vizdoom_scenario = vizdoom_path + "scenarios/locha_home_1.wad"
 
 if (lab):
     from env_lab import EnvLab
