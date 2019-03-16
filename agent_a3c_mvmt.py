77eee22ca74b64df93f715d64ebf05a7cd41dcdf
Yash Kulkarni <yculcarneee@gmail.com>
2019-03-17 00:13:07 +0530

    change scenario to sabsesasta1_1.wad

--- agent_a3c_mvmt.py
+++ agent_a3c_mvmt.py
@@ -24,7 +24,7 @@ test_display = False
 test_write_video = True
 path_work_dir = "rl_3d/"
 vizdoom_path = "ViZDoom/"
-vizdoom_scenario = vizdoom_path + "scenarios/sabsesasta1.wad"
+vizdoom_scenario = vizdoom_path + "scenarios/sabsesasta1_1.wad"
 
 if (lab):
     from env_lab import EnvLab
@@ -37,7 +37,7 @@ else:
 
 learning_rate = 0.00025
 device = "/cpu:0"
-num_workers = 3
+num_workers = 8
 t_max = 30
 frame_repeat =  4 #10  # 4
 gamma = 0.99
