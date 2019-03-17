a49e943606f9b88528419b2906493bbbdd55e0e3
Yash Kulkarni <yculcarneee@gmail.com>
2019-03-17 12:16:39 +0530

    change num_ep=30

--- agent_dqn_mvmt.py
+++ agent_dqn_mvmt.py
@@ -55,7 +55,7 @@ if (lab):
 if (not lab):
     from env_vizdoom_dqn_mvmt import EnvVizDoom
 
-    learning_rate = 0.0005
+    learning_rate = 0.00025
     discount_factor = 0.99
     step_num = int(5e5)
     replay_memory_size = int(1e6)
@@ -293,7 +293,7 @@ def Test(agent):
     posY.append('%')
 
     reward_total = 0
-    num_episodes = 10
+    num_episodes = 30
     ep_counter = 1
     reward_list = []
     ep_list = []
